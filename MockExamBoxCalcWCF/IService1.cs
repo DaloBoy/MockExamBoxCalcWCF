96bf4845343eee1aac6211c2ade22bd612ea65d4
Daniel Pilanat Østergaard <dani5905@edu.easj.dk>
2017-10-23 11:10:48 +0200

    Oprettet WCF Service App Tilføjet DataContracts i Iservice, Samt lavet metoderne i Service1.

--- MockExamBoxCalcWCF/IService1.cs
+++ MockExamBoxCalcWCF/IService1.cs
@@ -14,34 +14,17 @@ namespace MockExamBoxCalcWCF
     {
 
         [OperationContract]
-        string GetData(int value);
+        double GetVolume(double length, double width, double height);
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
+        double GetSide(double volume, double side1, double side2);
     }
 
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    //[DataContract]
+    //public class CompositeType
+    //{
+       
+    //}
 }
